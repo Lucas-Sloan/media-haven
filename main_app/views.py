7462b430abca8460420935c20703638c61886200
Knemaire <kannemaire@gmail.com>
2024-09-17 18:03:49 -0400

    migrations 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
- # views.py 
-
-
+def home(request):
+    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
