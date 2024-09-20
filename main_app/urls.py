04b5730f82f7527c22d7f6765a37eaae783c6ca0
Andrew <andrewchau1993@gmail.com>
2024-09-19 18:01:05 -0700

    git err don, jk moved alot of things to classbased views and had to implement fixes for the switch

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,37 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
     path('', views.Home.as_view(), name='home'),
-
-    path('dashboard/', views.dashboard, name='dashboard'),
-
-    path('media/', views.media_index, name='media_index'),
-
-    path('media/add/', views.add_media, name='add_media'),
-
-    path('media/edit/<int:id>/', views.edit_media, name='edit_media'),
-    
-    path('media/add/<str:media_type>/', views.add_media, name='add_media_by_type'),
-    
-    path('media/<int:id>/confirm-delete/', views.confirm_delete_media, name='confirm_delete_media'),
-
-    path('media/<int:id>/delete/', views.delete_media, name='delete_media'),
-
-    path('media/<int:id>/', views.view_media, name='view_media'),
-
-    path('media/<int:id>/reviews/', views.media_reviews, name='media_reviews'),
-    
-    path('media/<int:id>/reviews/add/', views.add_review, name='add_review'),
-    
-    path('media/reviews/edit/<int:review_id>/', views.edit_review, name='edit_review'),
-
-    path('favorites/', views.favorites, name='favorites'),
-
-    path('media/<int:id>/reviews/delete/<int:review_id>/', views.delete_review, name='delete_review'),
-
-    path('media/<str:media_type>/', views.media_filtered, name='media_filtered'),
-
-    path('media/<str:media_type>/<str:status>/', views.media_filtered_status, name='media_filtered_status'),
-
-] 
+    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('favorites/', views.FavoritesListView.as_view(), name='favorites'),
+    path('media/', views.MediaListView.as_view(), name='media_index'),
+    path('media/add/', views.MediaCreateView.as_view(), name='add_media'),
+    path('media/add/<str:media_type>/', views.MediaCreateView.as_view(), name='add_media_by_type'),
+    path('media/edit/<int:pk>/', views.MediaUpdateView.as_view(), name='edit_media'),
+    path('media/<int:pk>/confirm-delete/', views.MediaDeleteView.as_view(), name='confirm_delete_media'),
+    path('media/<int:pk>/delete/', views.MediaDeleteView.as_view(), name='delete_media'),
+    path('media/<int:pk>/', views.MediaDetailView.as_view(), name='view_media'),
+    path('media/<int:pk>/reviews/', views.MediaDetailView.as_view(), name='media_reviews'),
+    path('media/<int:pk>/reviews/add/', views.ReviewCreateView.as_view(), name='add_review'),
+    path('media/reviews/edit/<int:pk>/', views.ReviewUpdateView.as_view(), name='edit_review'),
+    path('media/<int:media_id>/reviews/delete/<int:pk>/', views.ReviewDeleteView.as_view(), name='delete_review'),
+    path('media/<str:media_type>/', views.MediaFilteredListView.as_view(), name='media_filtered'),
+    path('media/<str:media_type>/<str:status>/', views.MediaFilteredStatusView.as_view(), name='media_filtered_status'),
+]
